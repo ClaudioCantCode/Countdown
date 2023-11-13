f047ab1c54be821a53db968f3420279736cbaf92
ClaudioCantCode <antonioclaudiopepe@gmail.com>
2023-11-13 22:40:00 +0100

    Initial view

---
+++ CountdownNC1/CountdownNC1/CountdownNC1/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  CountdownNC1
+//
+//  Created by Antonio Claudio Pepe on 13/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var scale: CGFloat = 1.5
+    
+    var body: some View {
+            
+        Text("Countdowns")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .font(Font.custom("Comic Sans MS", size:20))
+       
+        Spacer()
+        
+        VStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                    .frame(width: 270, height: 70)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                
+                HStack {
+                    Image(systemName: "plus.circle.fill")
+                          .scaledToFit()
+                          .scaleEffect(scale)
+                          .foregroundColor(.white)
+                        
+                    Button("New Countdown") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .bold()
+                    .foregroundColor(.white)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                }
+                }
+        }
+        .padding()
+        }
+    }
+
+
+#Preview {
+    ContentView()
+}
---
+++ CountdownNC1/CountdownNC1/CountdownNC1/CountdownNC1App.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownNC1App.swift
+//  CountdownNC1
+//
+//  Created by Antonio Claudio Pepe on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CountdownNC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
